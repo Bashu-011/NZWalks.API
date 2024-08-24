11bccbba1b29587f5f143b2ff761828e4ae87301
Bashu-011 <bashu2609@gmail.com>
2024-08-24 12:34:46 +0300

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // my url will be http/localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] students = new string[] { "John", "James", "Jane", "Mia", "Fiona" };
+
+            return Ok(students);
+        }
+    }
+}
