a62f9ed1458c9c55122ab3c8fae8b83770faf74e
Bashu-011 <bashu2609@gmail.com>
2024-08-28 06:36:29 +0300

    Seeded data into the auth table, ran migrations

--- Data/NZWalksAuthDbContext.cs
+++ Data/NZWalksAuthDbContext.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace NZWalks.API.Data
 {
     public class NZWalksAuthDbContext : IdentityDbContext
     {
-        public NZWalksAuthDbContext(DbContextOptions options) : base(options)
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerId = "ac1d8de2-aca8-44ee-afdc-17f01729d7c6";
+            var writerId = "fd2aed60-3bae-4da9-bd3f-682e0373819c";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerId,
+                    ConcurrencyStamp = readerId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id=writerId,
+                    ConcurrencyStamp=writerId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
         }
 
     }
